7cfdb2ac07279211ebd895d80312bdf831a3db05
Chaan <rnchagwe27@gmail.com>
2024-06-04 22:19:02 +0800

    pause

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const 
+const prompt = require("prompt-sync") ();
+const answer = prompt("Would you like to play (y/n)? ");
+
+if (answer.toLowerCase() === "y") {
+    const answer2 = prompt("Choose a path. s for south / n for north: ")
+    if (answer.toLowerCase() === "s") {
+        
+    }
+} else {
+    console.log("You missed something interesting.")
+}
